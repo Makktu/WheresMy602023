7b5e9a777ca3e2659b812ce7a9e5a29b734cfc16
Makktu <johnmcna@gmail.com>
2023-06-05 00:59:53 +0100

    setting up

--- App.jsx
+++ App.jsx
@@ -1,19 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import HomeScreen from './src/screens/HomeScreen';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style='light' />
+      <View style={styles.container}>
+        <HomeScreen />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    height: '100%',
+    backgroundColor: '#181717',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const HomeScreen = () => {
+  return <Text style={{ fontSize: 30, color: 'orange' }}>Where's My 60?</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default HomeScreen;
